8c561974a4c92cdcff11ddeeffc24e0216e2d65e
Marcos <paladinmarcos@gmail.com>
2023-03-24 18:30:18 -0300

    ContactsManagerOOP

---
+++ Contact.java
@@ -0,0 +1,7 @@
+package Teste;
+
+public class Contact {
+    String name;
+    String email;
+    String phoneNumber;
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+package Teste;
+
+public class Main {
+    public static void main(String [] args) {
+        ContactsManager myContactManager = new ContactsManager();
+    }
+}
