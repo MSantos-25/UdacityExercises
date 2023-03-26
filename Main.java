3111e850de4f4686c09a22cf72232d96985cf696
Marcos <paladinmarcos@gmail.com>
2023-03-26 17:16:09 -0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 package ContactsManagerOOP;
 
-import java.util.jar.Attributes.Name;
-
 public class Main {
     public static void main(String [] args) {
         ContactsManager myContactsManager = new ContactsManager();
